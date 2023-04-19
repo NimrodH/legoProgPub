ac2da0aba50d47a8115524692c5de7c9067077d1
nimrod huberman <cconnect@netvision.net.i>
2023-04-19 08:45:31 +0300

    saveUserAction() to students.js

--- lego1.js
+++ lego1.js
@@ -188,20 +188,6 @@ function changeSky(skyPath) {
 
 }
 
-async function saveUserAction() {
-    let bodyData = {
-        'ActionType': "colorl",
-        'ActionDetails': "blue",
-        'actionId': "102",
-        'block': "b5",
-        'isTrueAction': "true",
-        'model': "A",
-        'step': "5",
-        'time': "22:00",
-        'user': "100"
-    }
-    var result = await postData(usersURL, bodyData);
-}
 
 async function saveModel() {
 
--- students.js
+++ students.js
@@ -1,1 +1,15 @@
 "use strict"
+async function saveUserAction() {
+    let bodyData = {
+        'ActionType': "colorl",
+        'ActionDetails': "blue",
+        'actionId': "102",
+        'block': "b5",
+        'isTrueAction': "true",
+        'model': "A",
+        'step': "5",
+        'time': "22:00",
+        'user': "101"
+    }
+    var result = await postData(usersURL, bodyData);
+}
