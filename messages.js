260945073f9db24e39a208ce30867553077743ba
nimrod huberman <cconnect@netvision.net.i>
2023-04-20 15:50:26 +0300

    new test

--- messages.js
+++ messages.js
@@ -3,12 +3,13 @@ class Messages {
 
     plane = BABYLON.Mesh.CreatePlane("plane", 10);
     advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateForMesh(this.plane);
-        constructor() {
+    currentScreen = "init";
+    constructor() {
 
         this.plane.position.z = -25;
         this.plane.position.y = 2;
         this.plane.position.x = 0;
-        this.plane.billboardMode = BABYLON.Mesh.BILLBOARDMODE_ALL;
+        //this.plane.billboardMode = BABYLON.Mesh.BILLBOARDMODE_ALL;
         this.advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateForMesh(this.plane);
 
         var button1 = BABYLON.GUI.Button.CreateSimpleButton("but1", "המשך");
@@ -17,10 +18,7 @@ class Messages {
         button1.color = "white";
         button1.fontSize = 50;
         button1.background = "green";
-        button1.onPointerUpObservable.add(function () {
-            //alert("you did it!");
-            //inputTextArea.text = "foo";
-        });
+        button1.onPointerUpObservable.add(this.nextScreen);
         button1.top = "60px";
         button1.left = "10px";
         button1.height = "70px";
@@ -33,7 +31,9 @@ class Messages {
         //var this.advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
         this.advancedTexture.background = 'green'
 
-        const initialText = " המסך הצבע על סימן המשקפיים בפינה הימנית תחתונה שלהמסך והפעל אותו. רק לאחר שאתה במצב מציאות מדומה, הקלק על כפתור המשך";// some example text. \nIt's quite long, so will need to be displayed across multiple lines. \n\nIt also has several line breaks.";
+        //const initialText = "במסך זה יופיעו הנחיות.\n מימנך נמצאת קוביה קטנה - זה המודל.\n מאחוריך מספר לבנים.\n עכשו יש להקליק על כפתור המשך.\n הקלקה פרושה להצביע עם הקרן וללחוץ על ההדק ";
+        const initialText = "בוקר טוב";
+        // some example text. \nIt's quite long, so will need to be displayed across multiple lines. \n\nIt also has several line breaks.";
         //this.advancedTexture.drawText(initialText)
         //this.advancedTexture.drawText(initialText, 1000, 500, "36px Arial", "black", "white", true, true);
 
@@ -46,21 +46,21 @@ class Messages {
         inputTextArea.fontSize = 48;
         inputTextArea.top = "-70px";
         inputTextArea.height = "70px";
-        this.advancedTexture.addControl(inputTextArea);
+        //this.advancedTexture.addControl(inputTextArea);
 
         const keyboard = new BABYLON.GUI.VirtualKeyboard();
         keyboard.addKeysRow(["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "\u2190"]);
         keyboard.connect(inputTextArea);
         keyboard.top = "10px";
         //keyboard.left = "10px";
-         this.advancedTexture.addControl(keyboard);
+       // this.advancedTexture.addControl(keyboard);
 
         const text1 = new BABYLON.GUI.TextBlock("upperText");
         text1.text = initialText;//"Hello world";
         text1.color = "red";
-        text1.fontSize = 24;
+        text1.fontSize = 36;
         text1.top = "-400px";
-        text1.height = "200px"
+        text1.height = "600px"
         this.advancedTexture.addControl(text1);
 
         //this.advancedTexture.focusedControl = inputTextArea;
@@ -68,6 +68,16 @@ class Messages {
         //plane.dispose();
         //console.log("but1: " + this.advancedTexture.getControlByName("but1").width);
 
+    }
+    nextScreen() {
+        switch (this.currentScreen) {
+            case "init":
+                this.currentScreen = "id_group";
+                break;
+        
+            default:
+                break;
+        }
     }
     showMessage(theText) {
         this.advancedTexture.getControlByName("upperText").text = theText;
