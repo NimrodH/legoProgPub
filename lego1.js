21eeb300abb4790aef8fcffc3ffe279017fd9775
nimrod huberman <cconnect@netvision.net.i>
2023-11-20 16:09:34 +0200

    near 0,0 follow true

--- lego1.js
+++ lego1.js
@@ -156,10 +156,10 @@ function createNearMenu(mode) {
     let near = new BABYLON.GUI.NearMenu("near");
     manager.addControl(near);
     let follower = near.defaultBehavior.followBehavior; //returns the followbehavior created by the 
-    near.defaultBehavior.followBehaviorEnabled = false;
+    near.defaultBehavior.followBehaviorEnabled = true;///false
     near.columns = 5;
     near.margin = 0.2
-    near.position = new BABYLON.Vector3(1, 1, -5);///1, 6, 1);
+    near.position = new BABYLON.Vector3(0, 0, -5);///1, 6, 1);
     near.isVisible = false;
     near.billboardMode = BABYLON.Mesh.BILLBOARDMODE_Y;
     near.backPlateMargin = 0.01;
